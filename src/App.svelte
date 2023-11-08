3e6f9c5225276d6522ffca2962e831229d70c54e
Fabian F <78906517+FabulousCodingFox@users.noreply.github.com>
2023-11-08 13:15:18 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Join from "./pages/Join.svelte";
+  import TileBackground from "./shared/TileBackground.svelte";
+</script>
+
+<div id="bg">
+  <TileBackground />
+</div>
+
+<main>
+  <Join />
+</main>

7395ab1317d2003b6cad36fdfbf07d5bf4dab2b9
Fabian F <78906517+FabulousCodingFox@users.noreply.github.com>
2023-11-08 19:42:59 +0100

    Button shortcut

--- src/pages/Join.svelte
+++ src/pages/Join.svelte
@@ -2,14 +2,29 @@
   import { _ } from "svelte-i18n";
   import logoSrc from "../assets/logo.svg";
   import LanguageSelector from "../shared/LanguageSelector.svelte";
+
+  let button: HTMLButtonElement;
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === "Enter") {
+      button.click();
+    }
+  };
 </script>
 
 <section class="fwh rel flex-center">
   <div class="join-container">
     <img class="noselect nointeract" src={logoSrc} alt="Logo" />
     <div class="form">
-      <input type="text" placeholder={$_("page.join.code")} />
-      <button class="button">{$_("page.join.button.next")}</button>
+      <input
+        type="text"
+        placeholder={$_("page.join.code")}
+        maxlength="6"
+        on:keydown={handleKeyDown}
+      />
+      <button class="button" bind:this={button}
+        >{$_("page.join.button.next")}</button
+      >
     </div>
   </div>
 </section>

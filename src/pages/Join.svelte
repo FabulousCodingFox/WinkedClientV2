82ad2a7ed65992bef19a997b5da4aa08a183269b
Fabian F <78906517+FabulousCodingFox@users.noreply.github.com>
2023-11-08 15:07:11 +0100

    Indenting CSS, adding logo & added HTML

--- src/pages/Join.svelte
+++ src/pages/Join.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
+  import logoSrc from "../assets/logo.svg";
 </script>
 
-<section>
-    
+<section class="fwh rel flex-center">
+  <div class="join-container">
+    <img src={logoSrc} alt="Logo" />
+    <form>
+      <input type="text" placeholder="Code" />
+      <button>Weiter</button>
+    </form>
+  </div>
 </section>
+
+<style lang="sass">
+.join-container
+  width: calc(100% - var(--gap))
+  max-width: 540px
+</style>

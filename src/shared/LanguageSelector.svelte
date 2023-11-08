40e97e1b2d956805bf985bae6b08b790ad26572e
Fabian F <78906517+FabulousCodingFox@users.noreply.github.com>
2023-11-08 16:50:31 +0100

    Create LanguageSelector.svelte

---
+++ src/shared/LanguageSelector.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { locale, locales, _ } from "svelte-i18n";
+
+  function setLocale(lang: string) {
+    locale.set(lang);
+  }
+</script>
+
+<div class="language-selector">
+  <button>
+    <!--Get the current locale and get the string 'path' from it and set it as a svg element-->
+    <span>{$locale}</span>
+  </button>
+  <div class="dropdown">
+    {#each $locales as lang}
+      <button on:click={() => locale.set(lang)}>
+        <span>{lang}</span>
+      </button>
+    {/each}
+  </div>
+</div>
+
+<style lang="sass">
+.language-selector
+  position: absolute
+  top: $GAP
+  right: $GAP
+</style>

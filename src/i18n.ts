c1ddd8215419882bbb62bfb254c8da3cf1b95948
Fabian F <78906517+FabulousCodingFox@users.noreply.github.com>
2023-11-08 16:50:01 +0100

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,10 @@
 import { addMessages, init, getLocaleFromNavigator } from 'svelte-i18n';
 
 import de from './lang/de.json';
+import en from './lang/en.json';
 
 addMessages('de', de);
+addMessages('en', en);
 
 init({
   fallbackLocale: 'de',

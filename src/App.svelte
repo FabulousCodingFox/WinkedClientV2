4fe7e3726373002e6c3400b7f7535920d91f53b8
Fabian F <78906517+FabulousCodingFox@users.noreply.github.com>
2023-11-08 22:33:38 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,18 @@
   $: pageJoinStage = 0;
 
   function onPageJoinCodeEnter(code: string): string {
+    if (code.length == 0) return "page.join.error.code-required";
+    if (code.length < 6) return "page.join.error.code-to-short";
+    if (code.length > 6) return "page.join.error.code-to-long";
+    if (!/^[a-zA-Z0-9]+$/.test(code)) return "page.join.error.code-invalid";
     return "";
   }
 
   function onPageJoinNameEnter(code: string): string {
+    if (code.length == 0) return "page.join.error.name-required";
+    if (code.length < 3) return "page.join.error.name-to-short";
+    if (code.length > 16) return "page.join.error.name-to-long";
+    if (!/^[a-zA-Z0-9]+$/.test(code)) return "page.join.error.name-invalid";
     return "";
   }
 </script>
